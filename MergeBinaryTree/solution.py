b5ae12c198d6143b0efdd8ffa745bc3074f20f45
Fischer-L <Fischer-L@users.noreply.github.com>
2017-09-01 01:52:32 +0800

    Update solution.py

--- MergeBinaryTree/solution.py
+++ MergeBinaryTree/solution.py
@@ -47,3 +47,56 @@ class Solution():
                root.right = TreeNode(rightVal)
                self._merge(right1, right2, root.right)
                
+class TestData:
+     def init():
+          TestData.createTree1()
+          TestData.createTree2()
+          TestData.createResultTree()
+          
+     def createTree1():
+          t = TreeNode(1)
+          left = t.left = TreeNode(3)
+          right = t.right = TreeNode(2)
+          left.left = TreeNode(5)
+          TestData.t1 = t
+
+     def createTree2():
+          t = TreeNode(2)
+          left = t.left = TreeNode(1)
+          right = t.right = TreeNode(3)
+          left.right = TreeNode(4)
+          right.right = TreeNode(7)
+          TestData.t2 = t
+          
+     def createResultTree():
+          t = TreeNode(3)
+          left = t.left = TreeNode(4)
+          right = t.right = TreeNode(5)
+          left.left = TreeNode(5)
+          left.right = TreeNode(4)
+          right.right = TreeNode(7)
+          TestData.result = t
+          
+     def compareTrees(t1, t2):
+          if t1 == None and t2 == None:
+               return True
+          
+          same = False
+          if t1.val == t2.val:
+               same = TestData.compareTrees(t1.left, t2.left)
+          if same:
+               same = TestData.compareTrees(t1.right, t2.right)
+          return same
+               
+     
+TestData.init()        
+same = TestData.compareTrees(TestData.t1, TestData.t2)
+print('TestData.t1 != TestData.t2: ' + ('True' if not same else 'False'))
+same = TestData.compareTrees(TestData.result, TestData.result)
+print('TestData.result == TestData.result: ' + ('True' if same else 'False'))
+
+s = Solution()
+same = TestData.compareTrees(TestData.result, s.mergeTrees(TestData.t1, TestData.t2))
+print('TestData.result == Solution result: ' + ('True' if same else 'False'))
+
+          
