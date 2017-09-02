c3fd9f3cc21161ed1c7c77f2c926aced1cf3aa68
Fischer-L <Fischer-L@users.noreply.github.com>
2017-09-02 21:04:29 +0800

    Update Solution.py

--- JudgeRouteCircle/Solution.py
+++ JudgeRouteCircle/Solution.py
@@ -4,3 +4,33 @@ class Solution:
         :type moves: str
         :rtype: bool
         """
+        originPos = [0, 0]
+        currentPos = [0, 0]
+        for m in moves:
+          _move = getattr(self, m)
+          _move(currentPos)
+          if currentPos[0] == originPos[0] and currentPos[1] == originPos[1]: return True
+        return False
+
+
+    def U(self, pos):
+      pos[0] += 1
+
+    def D(self, pos):
+      pos[0] -= 1
+
+    def R(self, pos):
+      pos[1] += 1
+
+    def L(self, pos):
+      pos[1] -= 1
+
+testMoves = []
+testMoves.append(('UD', True))
+testMoves.append(('LL', False))
+testMoves.append(('URDL', True))
+testMoves.append(('URDDL', False))
+
+s = Solution()
+for data in testMoves:
+  print('Circle: %s == Solution: %s of moves: %s' % (data[1], s.judgeCircle(data[0]), data[0]))
