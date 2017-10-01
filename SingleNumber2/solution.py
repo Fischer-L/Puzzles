77fc21107a48280201ca65458c7bdfa3000cc8a5
Fischer-L <Fischer-L@users.noreply.github.com>
2017-10-01 15:33:35 +0800

    Update solution.py

--- SingleNumber2/solution.py
+++ SingleNumber2/solution.py
@@ -11,3 +11,16 @@ class Solution(object):
           twos = (twos ^ i) & ~ones
         return ones
         
+    def singleNumberByCountingBits(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = 0
+        for bit in range(32):
+            sum = 0
+            for i in nums:
+                if (i >> bit) & 1: sum += 1
+            if sum % 3:
+                ans |= sum << bit
+        return ans
