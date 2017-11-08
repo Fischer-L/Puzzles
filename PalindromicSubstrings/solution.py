80e93a99a2a67f5a7edd49fba56c6bbe3229d314
Fischer-L <Fischer-L@users.noreply.github.com>
2017-11-08 23:24:58 +0800

    Update solution.py

--- PalindromicSubstrings/solution.py
+++ PalindromicSubstrings/solution.py
@@ -1,20 +1,48 @@
-# WIP....
 class Solution:
-    def __init__(self):
-        self._cache = {};
-    
     def countSubstrings(self, s):
         """
         :type s: str
         :rtype: int
         """
+        return self._countByLoopFromCenter(s)
+    
+    def _countByLoopFromCenter(self, s):
+        N = len(s)
+        i = j = -1
+        count = 0
+        
+        for x in range(N):
+            i = j = x
+            while i >= 0 and j < N:
+                if s[i] == s[j]:
+                    count += 1
+                    i -= 1
+                    j += 1
+                else:
+                    break
+                    
+        for x in range(N):
+            i = x
+            j = x + 1
+            while i >= 0 and j < N:
+                if s[i] == s[j]:
+                    count += 1
+                    i -= 1
+                    j += 1
+                else:
+                    break
+                    
+        return count
+    
+    def _countByDP(self, s):
+        self._cache = {};
         count = 0
         sLen = len(s)
         for b in range(sLen):
             for e in range(b, sLen):
                 if self._isPalindromic(s, b ,e): count += 1
         return count
-        
+    
     def _isPalindromic(self, s, b, e):
         """
         : type s: str
