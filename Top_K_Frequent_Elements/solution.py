34ad3a320650bb621d4ca2bf51e498850d8103bc
Fischer-L <Fischer-L@users.noreply.github.com>
2017-11-13 20:17:46 +0800

    Update solution.py

--- Top_K_Frequent_Elements/solution.py
+++ Top_K_Frequent_Elements/solution.py
@@ -7,30 +7,34 @@ class Solution:
         :type k: int
         :rtype: List[int]
         """
-        return self._goCounter(nums, k)
+        return self._goHeap(nums, k)
     
     def _goCounter(self, nums, k):
         coms = Counter(nums).most_common(k)
         return [ i[0] for i in coms ]
     
     def _goHeap(self, nums, k):
-        let heap = self._buildMaxHeap(nums)
-        return heap[:k]
+        c = Counter(nums)
+        heap = []
+        for i, v in enumerate(c): heap.append([v, c[v]])
+        self._buildMaxHeap(heap)
+        return [ i[0] for i in heap[:k]]
         
-    def _buildMaxHeap(self, nums):
-        n = len(nums)
+    def _buildMaxHeap(self, heap):
+        n = len(heap)
         ldx = int(n / 2) # the index of the last internal node        
         for i in range(ldx, 0, -1):
-            self._maxHeapify(nums, i, n)
+            self._maxHeapify(heap, i, n)
             
-    def _maxHeapify(self, nums, i, n):
+    def _maxHeapify(self, heap, i, n):
+        print(i)
         biggest = i - 1
         l = 2 * i - 1
         r = 2 * i
-        if nums[biggest] < nums[l]: biggest = l
-        if r < n and nums[biggest] < nums[r]: biggest = r
-        if biggest != i:
-            nums[biggest] = nums[biggest] ^ nums[i]
-            nums[i] = nums[biggest] ^ nums[i]
-            nums[biggest] = nums[biggest] ^ nums[i]
-            self._maxHeapify(nums, biggest, n)
+        if l < n and heap[biggest][1] < heap[l][1]: biggest = l
+        if r < n and heap[biggest][1] < heap[r][1]: biggest = r
+        if biggest != i - 1:
+          tmp = heap[i]
+          heap[i] = heap[biggest]
+          heap[biggest] = tmp
+          self._maxHeapify(heap, biggest, n)
