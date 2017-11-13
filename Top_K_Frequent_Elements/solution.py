d36a7e4e7e75fe7f35af97bc674a18713ec89057
Fischer-L <Fischer-L@users.noreply.github.com>
2017-11-13 19:59:52 +0800

    Update solution.py

--- Top_K_Frequent_Elements/solution.py
+++ Top_K_Frequent_Elements/solution.py
@@ -26,13 +26,23 @@ class Solution:
         return [ i[0] for i in coms ]
     
     def _goHeap(self, nums, k):
-        let mh = self._buildMaxHeap(nums)
+        let heap = self._buildMaxHeap(nums)
+        return heap[:k]
         
     def _buildMaxHeap(self, nums):
         n = len(nums)
         ldx = int(n / 2) # the index of the last internal node        
-        for i in range(ldx, -1, -1):
+        for i in range(ldx, 0, -1):
             self._maxHeapify(nums, i, n)
             
     def _maxHeapify(self, nums, i, n):
-        
+        biggest = i - 1
+        l = 2 * i - 1
+        r = 2 * i
+        if nums[biggest] < nums[l]: biggest = l
+        if r < n and nums[biggest] < nums[r]: biggest = r
+        if biggest != i:
+            nums[biggest] = nums[biggest] ^ nums[i]
+            nums[i] = nums[biggest] ^ nums[i]
+            nums[biggest] = nums[biggest] ^ nums[i]
+            self._maxHeapify(nums, biggest, n)
