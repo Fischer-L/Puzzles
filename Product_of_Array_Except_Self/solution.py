96a4506717d9908262b94a370c32d5f1e4c3f6a5
Fischer-L <Fischer-L@users.noreply.github.com>
2017-11-25 23:43:16 +0800

    Update solution.py

--- Product_of_Array_Except_Self/solution.py
+++ Product_of_Array_Except_Self/solution.py
@@ -4,6 +4,12 @@ class Solution:
         :type nums: List[int]
         :rtype: List[int]
         """
+        
+    def _solution1(self, nums):
+        """
+        Calculate all products then divide the i-th value for the output[i].
+        This solution has to watch out the zero value case
+        """
         zeroCount = 0
         allProducts = 1
         numsLen = len(nums)
@@ -21,3 +27,22 @@ class Solution:
         else:
             return [ int(allProducts/nums[i]) for i in range(numsLen) ]
         
+
+    def _solution2(self, nums):
+        """
+        First loop, for the output[i], calculate the products on the left side.
+        Second loop, for the output[i], calculate the products on the right side multiplied the products on the left side
+        """
+        numsLen = len(nums)
+        ans = [1] * numsLen
+        leftProducts = 1
+        for i in range(1, numsLen):
+           leftProducts = ans[i] = leftProducts * nums[i - 1]
+            
+        rightProducts = 1
+        for i in range(numsLen - 2, -1, -1):
+            rightProducts = rightProducts * nums[i + 1]
+            ans[i] *= rightProducts
+        
+        return ans
+    
