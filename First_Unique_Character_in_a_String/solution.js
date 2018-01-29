827396a1f4c73271dda9275e20eb450c9e4e1c60
Fischer-L <Fischer-L@users.noreply.github.com>
2018-01-29 11:40:04 +0800

    Create solution.js

---
+++ First_Unique_Character_in_a_String/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+  if (!s) {
+    return -1;
+  }
+  let dict = {};
+  for (let i = s.length - 1; i >= 0; --i) {
+      let c = s[i];
+      if (dict[c] === undefined) dict[c] = 0;
+      ++dict[c];
+  }
+  for (let i = 0; i < s.length; ++i) {
+    let c = s[i];
+    if (dict[c] == 1) return i;
+  }
+  return -1;
+};
