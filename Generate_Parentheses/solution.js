240cd9f65c54423ce77f4841aa0e579994810088
Fischer-L <Fischer-L@users.noreply.github.com>
2018-01-30 15:54:04 +0800

    Update solution.js

--- Generate_Parentheses/solution.js
+++ Generate_Parentheses/solution.js
@@ -3,48 +3,35 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-    let s = 0; // start
-    let e = (n * 2) - 1; // end
-    let ans = genParentheses(s, e);
+    let ans = genParentheses(n * 2);
     return ans;
 };
 
 var combinationCache = {};
 
-var genParentheses = function (s, e) {
-    if (s >= e)  return null;
+var genParentheses = function (n) {
+    if (n <= 1) return null;
+    if (combinationCache[n]) return combinationCache[n];
     
-    let comb = combinationCache[s + "-" + e];
-    if (comb) return comb;
+    let totalCombs = [];
+    for (let s = 0, m = 1; m < n; m += 2) {
+        let combs = [];
+        let frontCombs = genParentheses(m - s - 1);
+        if (frontCombs) {
+            frontCombs.forEach(c => combs.push(`(${c})`));
+        } else {
+            combs.push("()");
+        }
+        let tailCombs = genParentheses(n - m - 1);
+        if (tailCombs) {
+            combs = combs.reduce((newCombs, front) => {
+                tailCombs.forEach(c => newCombs.push(front + c));
+                return newCombs;
+            }, []);
+        }
+        totalCombs = totalCombs.concat(combs);
+    }
     
-    let frontCombinations = null;
-    let tailCombinations = null;
-    for (let i = s + 1; i < e; s += 2) {
-        frontCombinations = genParentheses(s, i);
-        if (e - i + 1 > 1) {
-            tailCombinations = genParentheses(i + 1, e);
-        }
-    }
-    comb = [];
-    if (frontCombinations) {
-        let len = frontCombinations.length - 1;
-        for (let i = 0; i < len; ++i) {
-            comb.push(`(${frontCombinations[i]})`);
-        }
-    }
-    if (tailCombinations) {
-        let buf = [];
-        let cLen = comb.length - 1;
-        let tLen = tailCombinations.length - 1;
-        for (let i = 0; i < cLen; ++i) {
-            let c = comb[i];
-            for (let j = 0; j < tLen; ++j) {
-                buf.push(c + tailCombinations[j]);
-            }
-        }
-        comb = buf;
-    }
-    if (comb.length == 0) comb = ["()"];
-    combinationCache[s + "-" + e] = comb;
-    return comb;
+    combinationCache[n] = totalCombs;
+    return totalCombs;
 }
