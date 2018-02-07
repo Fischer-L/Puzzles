625f8a3a5ac62684a9b3c6df9741c9601fcb5c7c
Fischer-L <Fischer-L@users.noreply.github.com>
2018-02-07 17:01:56 +0800

    Create solution.js

---
+++ License_Key_Formatting/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} S
+ * @param {number} K
+ * @return {string}
+ */
+var licenseKeyFormatting = function(S, K) {
+    let ans = [];
+    S = S.split("-").join("").toUpperCase();
+    console.log(S);
+    let i = S.length;
+    do {
+        i -= K;
+        if (i >= 0) {
+          ans.push(S.substr(i, K));
+        } else {
+            if (i + K > 0) ans.push(S.substr(0, i + K));
+        }
+    } while (i >= 0)
+    return ans.reverse().join("-");
+};
