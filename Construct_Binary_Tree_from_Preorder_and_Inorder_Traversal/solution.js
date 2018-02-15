0a53e282453a3db15cfec7077fef6026c3cdad77
Fischer-L <Fischer-L@users.noreply.github.com>
2018-02-15 17:48:36 +0800

    Create solution.js

---
+++ Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/solution.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    let ri = preorder.length > 0 ? inorder.indexOf(preorder[0]) : -1;
+    if (ri < 0) return null;
+    
+    let root = {
+        val: preorder[0]
+    };
+    
+    let leftPreorder = preorder.slice(1, ri + 1);
+    let leftInorder = inorder.slice(0, ri);
+    root.left = buildTree(leftPreorder, leftInorder);
+    
+    let rightPreorder = preorder.slice(ri + 1);
+    let rightInorder = inorder.slice(ri + 1);
+    root.right = buildTree(rightPreorder, rightInorder);
+    console.log(root);
+    return root;
+};
