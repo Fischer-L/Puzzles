9cd7a90cd3b15413f2894cb52ca9c57b9b3a3940
Fischer-L <Fischer-L@users.noreply.github.com>
2018-02-24 01:23:45 +0800

    Update solution.js

--- Merge_Sorted_Array/solution.js
+++ Merge_Sorted_Array/solution.js
@@ -6,14 +6,24 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let i = 0, j = 0;
-    while (j < n) {
-        let v1 = nums1[i];
-        let v2 = nums2[j];
-        if (v1 >= v2) {
-            nums1.splice(i, 0, v2);
-            ++j;
+    let i = m - 1;
+    let j = n - 1;
+    let top = m + n - 1;
+    while (i >= 0 && j >= 0) {
+        let a = nums1[i];
+        let b = nums2[j];
+        if (a >= b) {
+            nums1[top] = a;
+            --i;
+        } else {
+            nums1[top] = b;
+            --j;
         }
-        if (i < nums1.length - 1) ++i;
+        --top;
+    }
+    while (j >= 0) {
+        nums1[top] = nums2[j];
+        --j;
+        --top;
     }
 };
