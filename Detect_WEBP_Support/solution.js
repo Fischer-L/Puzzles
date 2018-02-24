fc97557bbe377cfae4b1d4a5c3321a7a112c4a2e
Fischer-L <Fischer-L@users.noreply.github.com>
2018-02-24 17:58:37 +0800

    Update solution.js

--- Detect_WEBP_Support/solution.js
+++ Detect_WEBP_Support/solution.js
@@ -1,3 +1,15 @@
 function okWebP() {
-
+  return new Promise(resolve => {
+    if (okWebP._ok !== undefined) resolve(okWebP._ok);
+    let img = new Image();
+    img.onload = () => {
+      okWebP._ok = true;
+      resolve(true);
+    };
+    img.onerror = () => {
+      okWebP._ok = false;
+      resolve(false);
+    };
+    img.src = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+  });  
 }
