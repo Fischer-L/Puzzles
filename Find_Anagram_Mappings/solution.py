6852b3e97cb69d3bf6b58c9181697de064bfac0b
Fischer-L <Fischer-L@users.noreply.github.com>
2018-04-21 20:44:04 +0800

    Update solution.py

--- Find_Anagram_Mappings/solution.py
+++ Find_Anagram_Mappings/solution.py
@@ -1,1 +1,15 @@
 def findAnagram(A, B):
+  L = len(B)
+  if L == 0: return None
+  
+  mapB = {}
+  for i in range(L):
+    b = B[i]
+    if b not in mapB: mapB[b] = i
+    
+  anaMap = []
+  for i in range(L):
+    a = A[i]
+    anaMap.append(mapB[a])
+    
+  return anaMap
