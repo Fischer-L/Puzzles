24d0e370ef6a3496eed464b5f9045dbefc196831
Fischer-L <Fischer-L@users.noreply.github.com>
2018-04-21 20:33:08 +0800

    Update solution.js

--- Find_Anagram_Mappings/solution.js
+++ Find_Anagram_Mappings/solution.js
@@ -1,3 +1,19 @@
 function findAnagram(A, B) {
-
+  let len = B.length;
+  if (len === 0) return null;
+  
+  let mapB = {};
+  for (let i = 0; i < len; ++i) {
+    let b = B[i];
+    if (mapB[b] === undefined) {
+      mapB[b] = i;
+    }
+  }
+  
+  let anaMap = [];
+  for (let i = 0; i < len; ++i) {
+    let a = A[i];
+    anaMap.push(mapB[a]);
+  }
+  return anaMap;
 }
