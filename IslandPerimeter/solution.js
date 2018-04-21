2e3f4a7f9a962370fd5c98f8657f0ebdd98e54e6
Fischer-L <Fischer-L@users.noreply.github.com>
2018-04-21 21:09:20 +0800

    Update solution.js

--- IslandPerimeter/solution.js
+++ IslandPerimeter/solution.js
@@ -1,3 +1,39 @@
 function calcIsIandPerimeter(island) {
-
+  const V = island.length; // Max vertical
+  const H = V ? island[0].length : 0 // Max horizontal
+  
+  if (!V || !H) return -1;
+  
+  let peri = 0;
+  let land = false;
+  for (let i = 0; i < V; ++i) {
+    for (let j = 0; j < H; ++j) {
+      let cell = island[i][j];
+      if (cell === 1) {
+        if (!land) {
+          land = true;
+          peri += 2;
+        }
+      } else {
+        land = false
+      }
+    }
+  }
+  
+  land = false;
+  for (let i = 0; i < H; ++i) {
+    for (let j = 0; j < V; ++j) {
+      let cell = island[i][j];
+      if (cell === 1) {
+        if (!land) {
+          land = true;
+          peri += 2;
+        }
+      } else {
+        land = false
+      }
+    }
+  }
+  
+  return peri;
 }
