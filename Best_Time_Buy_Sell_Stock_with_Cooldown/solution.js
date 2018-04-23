0afa1207cc6ededd739558d85954e689a7251f15
Fischer-L <Fischer-L@users.noreply.github.com>
2018-04-23 18:18:16 +0800

    Update solution.js

--- Best_Time_Buy_Sell_Stock_with_Cooldown/solution.js
+++ Best_Time_Buy_Sell_Stock_with_Cooldown/solution.js
@@ -1,2 +1,14 @@
 function maxProfit(prices) {
+  let buy = -prices[0];
+  let sell = 0;
+  let cool = 0;
+  let prevBuy = 0;
+  let prevSell = 0;
+  for (let p of prices) {
+    prevBuy = buy;
+    buy = Math.max(prevBuy, prevSell - p);
+    prevSell = sell;
+    sell = Math.max(prevSell, prevBuy + p);
+  }
+  return Math.max(buy, sell);
 }
