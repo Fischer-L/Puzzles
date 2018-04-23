628614076e9d2a07abbf750e73a7984a881291b6
Fischer-L <Fischer-L@users.noreply.github.com>
2018-04-23 16:09:52 +0800

    Update solution.py

--- Climbing_Stairs/solution.py
+++ Climbing_Stairs/solution.py
@@ -1,2 +1,12 @@
 def climbStairs(n):
-
+  if n == 1 or n == 2: return n
+  
+  curr = 0
+  oneBelow = 2
+  twoBelow = 1
+  while n > 2:
+    curr = oneBelow + twoBelow
+    oneBelow = curr
+    twoBelow = oneBelow
+    n -= 1
+  return curr
