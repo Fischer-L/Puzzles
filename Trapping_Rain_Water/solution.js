d50bb1e5eb6abc85fed7f56eb37fdae3c5364a36
Fischer-L <Fischer-L@users.noreply.github.com>
2018-04-25 21:35:39 +0800

    Update solution.js

--- Trapping_Rain_Water/solution.js
+++ Trapping_Rain_Water/solution.js
@@ -1,2 +1,27 @@
 function trapWater(arr) {
+  let water = 0;
+  let maxLeft = 0;
+  let maxRight = 0;
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
+  while (leftIdx < rightIdx) {
+    let left = arr[leftIdx];
+    let right = arr[rightIdx];
+    if (left <= right) {
+      if (left >= maxLeft) {
+        maxLeft = left;
+      } else {
+        water += maxLeft - left;
+      }
+      leftIdx++;
+    } else {
+      if (right >= maxRight) {
+        maxRight = right;
+      } else {
+        water += maxRight - right;
+      }
+      rightIdx--;
+    }
+  }
+  return water;
 }
