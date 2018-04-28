4ecbf453489fe05a54f9dd8ba3ebff5f8d6111c3
Fischer-L <Fischer-L@users.noreply.github.com>
2018-04-29 00:11:46 +0800

    Update solution.py

--- Burst_Balloons/solution.py
+++ Burst_Balloons/solution.py
@@ -36,3 +36,28 @@ class Solution:
         
         return maxCoin
         
+    def maxCoinsByLoop(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        balloons = [1]
+        for n in nums:
+            if n > 0: balloons.append(n)
+        balloons.append(1)
+        
+        L = len(balloons)
+        dp = [ [0] * L for i in range(L) ]
+        
+        for k in range(2, L):
+            s = 0
+            e = s + k
+            while e < L:
+                for i in range(s+1, e):
+                    left = dp[s][i]
+                    right = dp[i][e]
+                    mid = balloons[s] * balloons[i] * balloons[e]
+                    dp[s][e] = max(dp[s][e], left + mid + right)
+                s += 1
+                e += 1
+        return dp[0][L-1]
