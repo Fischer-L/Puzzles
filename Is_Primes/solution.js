3e471adec8ba5bcec5458c0c25dcc733b34e745a
Fischer-L <Fischer-L@users.noreply.github.com>
2018-05-01 17:49:54 +0800

    Update solution.js

--- Is_Primes/solution.js
+++ Is_Primes/solution.js
@@ -4,12 +4,9 @@
 //   - where i = 1, 7, 11, 13, 17, 23, 27, 29
 // So we can test an number larger than 30 with the above form.
 function isPrimes(n) {
-  const trailingI = [1, 7, 11, 13, 17, 23, 27, 29];
-  
   if (n <= 30) {
-    const primes = [2, 3, 5, ...trailingI];
-    return primes.includes(n);
+    return [2, 3, 5, 7, 11, 13, 17, 23, 27, 29].includes(n);
   } else {
-    return trailingI.includes(n % 30);
+    return [1, 7, 11, 13, 17, 23, 27, 29].includes(n % 30);
   }
 }
