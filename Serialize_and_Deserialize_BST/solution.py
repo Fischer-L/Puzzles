75ee90e4e66150dfc184fd37724012e222050e43
Fischer-L <Fischer-L@users.noreply.github.com>
2018-05-01 16:03:58 +0800

    Create solution.py

---
+++ Serialize_and_Deserialize_BST/solution.py
@@ -0,0 +1,70 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec: 
+    
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if root == None: return ""
+        
+        treeStr = ""
+        parents = [root]
+        children = None
+        bottom = False
+        while not bottom:
+            bottom = True
+            children = []
+            for node in parents:
+                val = str(node.val) if node else "n"
+                treeStr += ("," + val)
+                left = None
+                if node and node.left:
+                    bottom = False
+                    left = node.left
+                right = None
+                if node and node.right:
+                    bottom = False
+                    right = node.right
+                children.append(left)
+                children.append(right)
+            parents = children
+        
+        return treeStr[1:]
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data == "": return None
+        
+        vals = data.split(",")
+        L = len(vals)
+        nodes = [None] * L
+        nodes[0] = TreeNode(int(vals[0]))
+        for i in range(L):
+            left = 2 * i + 1
+            right = 2 * i + 2
+            if right >= L: break
+                
+            node = nodes[i]
+            if node:
+                node.left = TreeNode(int(vals[left])) if vals[left] != "n" else None
+                node.right = TreeNode(int(vals[right])) if vals[right] != "n" else None
+                nodes[left] = node.left
+                nodes[right] = node.right
+        return nodes[0]
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
