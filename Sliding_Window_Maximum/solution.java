199572d12d9c52181b181e04772bdda8252792c7
Fischer-L <Fischer-L@users.noreply.github.com>
2018-05-02 20:42:59 +0800

    Update solution.java

--- Sliding_Window_Maximum/solution.java
+++ Sliding_Window_Maximum/solution.java
@@ -1,4 +1,24 @@
 class Solution {
+  
+  public int[] maxSlidingWindow_O_n(int[] nums, int k) {
+    final int L = nums.length;
+    if (L == 0 || L < k) return new int[0];
+
+    int wIdx = 0;
+    int[] window = new int[L - k + 1];
+    ArrayDeque<Integer> q = new ArrayDeque<Integer>();
+    
+    for (int i = 0; i < L; ++i) {
+      while (!q.isEmpty() && q.peekFirst() < i - k + 1) q.pollFirst();
+      int n = nums[i];
+      while (!q.isEmpty() && n >= nums[q.peekLast()]) q.pollLast();
+      q.addLast(i);
+      if (i >= k-1) window[wIdx++] = nums[q.peekFirst()];
+    }
+
+    return window;      
+  }
+  
   public int[] maxSlidingWindow_O_nk(int[] nums, int k) {
     final int L = nums.length;
     if (L == 0 || L < k) return new int[0];
