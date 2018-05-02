26e2542010280d49474e61e431b3019c2f8fc7f0
Fischer-L <Fischer-L@users.noreply.github.com>
2018-05-02 15:57:23 +0800

    Update solution.java

--- Max_Heap/solution.java
+++ Max_Heap/solution.java
@@ -1,8 +1,10 @@
 import java.util.*;
 
 class Solution {
-  public int[] buildMaxHeap(int[] nums) {
-    int[] heap = Arrays.copyOfRange(nums, 0, nums.length);
+  public List<Integer> buildMaxHeap(int[] nums) {
+    ArrayList<Integer> heap = new ArrayList<Integer>();
+    for (int n : nums) heap.add(n);
+    
     final int L = nums.length;
     int i = (int) Math.floor(L/2);
     for (; i >= 0; --i) {
@@ -11,25 +13,40 @@ class Solution {
     return heap;
   }
   
-  public void maxHeapify(int[] heap, int i, int ceil) {
+  public void maxHeapify(List<Integer> heap, int i, int ceil) {
     int maxIdx = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
     
     if (left < ceil && right < ceil) {
-      int bigger = heap[left] > heap[right] ? left : right;
-      if (heap[bigger] > heap[maxIdx]) maxIdx = bigger;
+      int bigger = heap.get(left) > heap.get(right) ? left : right;
+      if (heap.get(bigger) > heap.get(maxIdx)) maxIdx = bigger;
     } else if (left < ceil) {
-      if (heap[left] > heap[maxIdx]) maxIdx = left;
+      if (heap.get(left) > heap.get(maxIdx)) maxIdx = left;
     } else if (right < ceil) {
-      if (heap[right] > heap[maxIdx]) maxIdx = right;
+      if (heap.get(right) > heap.get(maxIdx)) maxIdx = right;
     }
     
     if (i != maxIdx) {
-      int less = heap[i];
-      heap[i] = heap[maxIdx];
-      heap[maxIdx] = less;
+      int less = heap.get(i);
+      heap.set(i, heap.get(maxIdx));
+      heap.set(maxIdx, less);
       this.maxHeapify(heap, maxIdx, ceil);
     }
+  }
+  
+  public Integer extractMaxHeap(List<Integer> heap) {
+    final int L = heap != null ? heap.size() : 0;
+    if (L == 0) return null;
+    
+    Integer max = heap.get(0);
+    if (L == 1) {
+      heap.remove(0);
+    } else {
+      heap.set(0, heap.get(L-1));
+      heap.remove(L-1);
+      this.maxHeapify(heap, 0, L-1);
+    }
+    return max;
   }
 }
