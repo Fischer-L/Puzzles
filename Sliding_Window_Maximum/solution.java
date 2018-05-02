92c49e5b6aa149c6c8a3a369b95b54f80acd6bea
Fischer-L <Fischer-L@users.noreply.github.com>
2018-05-02 14:13:20 +0800

    Create solution.java

---
+++ Sliding_Window_Maximum/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+  public int[] maxSlidingWindow_O_nk(int[] nums, int k) {
+    final int L = nums.length;
+    if (L == 0 || L < k) return new int[0];
+   
+    ArrayList<Integer> window = new  ArrayList<Integer>();
+    int s = 0;
+    int e = k-1;
+    window.add(this.findMax(nums, s, e));
+    int wSize = 1;
+    while (e < L-1) {
+      int max = window.get(wSize-1);
+      s++;
+      e++;
+      if (max == nums[s-1]) {
+        max = this.findMax(nums, s, e);
+      } else if (max < nums[e]) {
+        max = nums[e];
+      }
+      window.add(max);
+      wSize++;
+    }
+    int[] maxNums = new int[wSize];
+    // for (int i = 0; i < wSize; ++i) maxNums[i] = window.get(i);
+    window.toArray(maxNums);
+    return maxNums;
+  }
+
+  private int findMax(int[] nums, int s, int e) {
+    int max = nums[s];
+    for (int i = s; i <= e; ++i) {
+       if (max < nums[i]) max = nums[i];
+    }
+    return max;
+  }
+}
