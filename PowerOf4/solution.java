43b7abf70552c07dd30bf5e9d25241cd25806531
Fischer-L <Fischer-L@users.noreply.github.com>
2018-05-07 22:31:21 +0800

    Update solution.java

--- PowerOf4/solution.java
+++ PowerOf4/solution.java
@@ -1,6 +1,6 @@
 class Solution {
   public boolean isPowerOf4(int n) {
-    return this.isPowerOf2(n) && (n-1) % 4 == 3;
+    return this.isPowerOf2(n) && (n-1) % 3 == 0;
   }
   
   public boolean isPowerOf2(int n) {
