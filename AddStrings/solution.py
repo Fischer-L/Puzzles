1d7a8a44d14859a9562af87c60ca084ca7ab2518
Fischer-L <Fischer-L@users.noreply.github.com>
2018-05-12 21:19:41 +0800

    Update solution.py

--- AddStrings/solution.py
+++ AddStrings/solution.py
@@ -1,26 +1,22 @@
-class Solution(object):
-    def addStrings(self, num1, num2):
+class Solution:
+    def addStrings(self, a, b):
         """
         :type num1: str
         :type num2: str
         :rtype: str
         """
-        ans = ''
-        num1 = num1[::-1] # Revrese string so the index 0 == the least digit
-        num2 = num2[::-1]
-        num1Len = len(num1)
-        num2Len = len(num2)
-        added = extra = 0
-        i = 0
-        while i < num1Len or i < num2Len:
-            added = extra
-            if i < num1Len: added += int(num1[i])
-            if i < num2Len: added += int(num2[i])
-            extra = added // 10
-            # Construct the answer with the index 0 == the max digit
-            # ao that won't have to reverse string again
-            ans = str(added % 10) + ans
-            i += 1
-        return ans if extra == 0 else str(extra) + ans
-        
-        
+        i = len(a) - 1
+        j = len(b) - 1
+        ans = ""
+        carry = 0
+        while i >= 0 or j >= 0:
+            if i >= 0: 
+                carry += ord(a[i]) - ord('0')
+                i -= 1
+            if j >= 0:
+                carry += ord(b[j]) - ord('0')
+                j -= 1
+            ans = str(carry % 10) + ans
+            carry = 1 if carry >= 10 else 0
+        if carry > 0: ans = "1" + ans
+        return ans
