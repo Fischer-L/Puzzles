394e59e2bdb0ed3ee535b4640bdb7cd90682ace6
Fischer-L <Fischer-L@users.noreply.github.com>
2018-06-29 19:28:32 +0800

    Update solution.java

--- Word_Search_II/solution.java
+++ Word_Search_II/solution.java
@@ -2,8 +2,32 @@ class Solution {
     public List<String> findWords(char[][] board, String[] words) {
         ArrayList<String> matches = new ArrayList<String>();
         TrieNode root = TrieNode.buildTrieTree(words);
-        
+        final int MAX_ROW = board.length;
+        final int MAX_COL = board[0].length;
+        for (int r = 0; r < MAX_ROW; r++) {
+            for (int c = 0; c < MAX_COL; c++) {
+                this.searchTrieTree(matches, root, board, r, c, MAX_ROW, MAX_COL);
+            }
+        }
         return matches;
+    }
+    
+    public void searchTrieTree(ArrayList<String> matches, TrieNode node, char[][] board, int r, int c, int MAX_ROW, int MAX_COL) {
+        char chr = board[r][c];
+        TrieNode next = node.children.get(chr);
+        if (next == null) return;
+        
+        if (next.word != null) {
+            matches.add(next.word);
+            next.word = null;
+        }
+        
+        board[r][c] = ' ';
+        if (r-1 >= 0) this.searchTrieTree(matches, next, board, r-1, c, MAX_ROW, MAX_COL);
+        if (r+1 < MAX_ROW) this.searchTrieTree(matches, next, board, r+1, c, MAX_ROW, MAX_COL);
+        if (c-1 >= 0) this.searchTrieTree(matches, next, board, r, c-1, MAX_ROW, MAX_COL);
+        if (c+1 < MAX_COL) this.searchTrieTree(matches, next, board, r, c+1, MAX_ROW, MAX_COL);
+        board[r][c] = chr;
     }
 }
 
@@ -16,16 +40,16 @@ class TrieNode {
     public static TrieNode buildTrieTree(String[] words) {
         TrieNode root = new TrieNode();
         for (String word : words) {
-            TrieNode parent = root;
+            TrieNode node = root;
             final int L = word.length();
             for (int i = 0; i < L; ++i) {
                 char c = word.charAt(i);
-                if (!parent.children.containsKey(c)) {
-                    parent.children.put(c, new TrieNode());
+                if (!node.children.containsKey(c)) {
+                    node.children.put(c, new TrieNode());
                 }
-                parent = parent.children.get(c);
+                node = node.children.get(c);
             }
-            if (parent.word == null) parent.word = word;
+            node.word = word;
         }
         return root;
     }
