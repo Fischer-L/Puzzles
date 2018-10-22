2e4c6c4d9a5dac3dfdf0fe866f09c2a0a315060e
Fischer-L <Fischer-L@users.noreply.github.com>
2018-10-22 16:37:41 +0800

    Update solution.java

--- Sort_List/solution.java
+++ Sort_List/solution.java
@@ -7,36 +7,38 @@
  * }
  */
 class Solution {
-  public ListNode sortList(ListNode head) {
-    this.quickSort(head, null);
-    return head;
-  }
-
-  public void quickSort(ListNode head, ListNode end) {
-    if (head == end || head == null || head.next == null) return;
-    
-    ListNode curr = head.next;
-    ListNode pivot = head;
-    int pivotVal = head.val;
-    
-    while (curr != end) {
-      if (curr.val < pivotVal) {
-        int tmp = curr.val;
-        pivot = pivot.next;
-        curr.val = pivot.val;
-        pivot.val = tmp;
-      }
-      curr = curr.next;
+    public ListNode sortList(ListNode head) {
+      this.quickSortList(head, null);
+      return head;
     }
-    
-    head.val = pivot.val;
-    pivot.val = pivotVal;
-    
-    this.quickSort(head, pivot);
-    // Since we put those nodes with the same vals as the pivot on the right side
-    // there might be a chance we could skip those dulplicates.
-    while (pivot.next != null && pivot.val == pivot.next.val) pivot = pivot.next; 
-    this.quickSort(pivot.next, end);
-  }
 
+    private void quickSortList(ListNode head, ListNode end) {
+      if (head == end) return;
+
+      ListNode next = head.next;
+      ListNode pivot = head; // The node < the head in the value
+
+      while (next != end) {
+        if (next.val < head.val) {
+          pivot = pivot.next;
+          this.swapVal(pivot, next);
+        }
+        next = next.next;
+      }
+      this.swapVal(pivot, head);
+
+      this.quickSortList(head, pivot);  
+      // Since we put those nodes with the same vals as the pivot on the right side
+      // there might be a chance we could skip those dulplicates.
+      while (pivot.next != null && pivot.val == pivot.next.val) {
+        pivot = pivot.next;
+      }
+      this.quickSortList(pivot.next, end);
+    }
+
+    private void swapVal(ListNode a, ListNode b) {
+      int cache = a.val;
+      a.val = b.val;
+      b.val = cache;
+    }
 }
