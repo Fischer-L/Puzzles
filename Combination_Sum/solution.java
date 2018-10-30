37cabe4babd9b0d6783cfc80d9a832c8674d24d1
Fischer-L <Fischer-L@users.noreply.github.com>
2018-10-30 14:29:50 +0800

    Create solution.java

---
+++ Combination_Sum/solution.java
@@ -0,0 +1,37 @@
+class Solution {
+  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    final int L = candidates.length;
+    Arrays.sort(candidates);
+  	ArrayList<List<Integer>> ans = new ArrayList<List<Integer>>();
+    for (int i = 0; i < L; i++) {
+      ArrayList<List<Integer>> combo = this.sumCombo(candidates, i, L, target);
+      if (combo != null) ans.addAll(combo);
+    }
+    return ans;
+  }
+  
+  private ArrayList<List<Integer>> sumCombo(int[] candidates, int s, int e, int target) {
+    int prefix = candidates[s];
+  	target -= prefix;
+    
+    if (target < 0) return null;
+    
+    if (target == 0) {
+      ArrayList<List<Integer>> combo = new ArrayList<List<Integer>>();
+      combo.add(new ArrayList<Integer>(prefix));
+      return combo;
+    }
+ 	
+    ArrayList<List<Integer>> combo = new ArrayList<List<Integer>>();
+    for (int i = s; i < e; ++i) {
+      ArrayList<List<Integer>> subCombo = this.sumCombo(candidates, i, e, target);
+      if (subCombo != null) {
+        for (int j = subCombo.size() - 1; j >= 0; j--) subCombo[j].add(prefix);
+        combi.addAll(subCombo);
+      } else if (candidates[i] > target) {
+      	break;
+      }
+    }
+    return combo.size() > 0 ? combo : null;
+  }
+}
