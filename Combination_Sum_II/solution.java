a2c9a69bb522ff61345fdfa7c71f00ccfd35ad4a
Fischer-L <Fischer-L@users.noreply.github.com>
2018-11-08 18:58:48 +0800

    Update solution.java

--- Combination_Sum_II/solution.java
+++ Combination_Sum_II/solution.java
@@ -1,9 +1,12 @@
-
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-      ArrayList<List<Integer>> ans = new ArrayList<>();
-      Arrays.sort(candidates);
-      this.findCombinationa(candidates, 0, candidates.length, ans, new ArrayList<Integer>());
-      return ans;
-    }
+  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    ArrayList<List<Integer>> ans = new ArrayList<>();
+    Arrays.sort(candidates);
+    this.findCombinationa(target, candidates, 0, candidates.length, ans, new ArrayList<Integer>());
+    return ans;
+  }
+  
+  private void findCombinationa(int target, int[] candidates, int s, int e, ArrayList<List<Integer>> ans, ArrayList<Integer> combo) {
+  	
+  }
 }
