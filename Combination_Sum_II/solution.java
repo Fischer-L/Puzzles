4a64f230874f9e1321427757405402f1a920f801
Fischer-L <Fischer-L@users.noreply.github.com>
2018-11-08 19:23:15 +0800

    Update solution.java

--- Combination_Sum_II/solution.java
+++ Combination_Sum_II/solution.java
@@ -7,6 +7,22 @@ class Solution {
   }
   
   private void findCombinationa(int target, int[] candidates, int s, int e, ArrayList<List<Integer>> ans, ArrayList<Integer> combo) {
-  	
+    int v = candidates[s];
+  	target -= v;
+    
+    if (target < 0) return;
+    
+    combo.add(v);
+    
+    if (target == 0) {
+      ans.add(combo.clone());
+      return;
+    }
+    
+    for (int i = s; i < e; i++) {
+      if (i != s && candidates[i] == candidates[i-1]) {
+      	continue;
+      }
+    }
   }
 }
