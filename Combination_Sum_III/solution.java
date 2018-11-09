30f49343c89a0ea2ee1ce30ca0017eac28897eb2
Fischer-L <Fischer-L@users.noreply.github.com>
2018-11-09 17:36:28 +0800

    Create solution.java

---
+++ Combination_Sum_III/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    ArrayList<List<Integer>> ans = new ArrayList<>();
+    Arrays.sort(candidates);
+    this.findCombinationa(target, candidates, 0, candidates.length, ans, new ArrayList<Integer>());
+    return ans;
+  }
+  
+  private void findCombinationa(int target, int[] candidates, int s, int e, ArrayList<List<Integer>> ans, ArrayList<Integer> combo) {
+    if (target < 0) return;
+    
+    if (target == 0) {
+      ans.add((ArrayList) combo.clone());
+      return;
+    }
+    
+    for (int i = s; i < e; i++) {
+      int v = candidates[i];
+      
+      if (v > target) break;
+      
+      if (i != s && v == candidates[i-1]) {
+      	continue;
+      }
+      
+      combo.add(v);
+      this.findCombinationa(target - v, candidates, i + 1, e, ans, combo);
+      combo.remove(combo.size()-1);
+    }
+  }
+}
