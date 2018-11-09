a6b700514c0ba59ef290160ae20b4c60b9f2fc0f
Fischer-L <Fischer-L@users.noreply.github.com>
2018-11-09 16:09:19 +0800

    Update solution.java

--- Combination_Sum_II/solution.java
+++ Combination_Sum_II/solution.java
@@ -7,12 +7,7 @@ class Solution {
   }
   
   private void findCombinationa(int target, int[] candidates, int s, int e, ArrayList<List<Integer>> ans, ArrayList<Integer> combo) {
-    int v = candidates[s];
-  	target -= v;
-    
     if (target < 0) return;
-    
-    combo.add(v);
     
     if (target == 0) {
       ans.add(combo.clone());
@@ -20,9 +15,17 @@ class Solution {
     }
     
     for (int i = s; i < e; i++) {
-      if (i != s && candidates[i] == candidates[i-1]) {
+      int v = candidates[i];
+      
+      if (v > target) break;
+      
+      if (i != s && v == candidates[i-1]) {
       	continue;
       }
+      
+      combo.add(v);
+      this.findCombinationa(target - v, candidate, i + 1, e, ans, combo);
+      combo.remove(combo.size()-1);
     }
   }
 }
