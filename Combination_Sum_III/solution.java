0606f4c8fa9099a8b711b19ebc16a5a6dd4af789
Fischer-L <Fischer-L@users.noreply.github.com>
2018-11-09 17:45:31 +0800

    Update solution.java

--- Combination_Sum_III/solution.java
+++ Combination_Sum_III/solution.java
@@ -1,30 +1,36 @@
 class Solution {
-  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+  public List<List<Integer>> combinationSum3(int k, int n) {
     ArrayList<List<Integer>> ans = new ArrayList<>();
-    Arrays.sort(candidates);
-    this.findCombinationa(target, candidates, 0, candidates.length, ans, new ArrayList<Integer>());
+    ArrayList candidates = this.createCandidates(n);
+    this.findCombinationa(k, n, candidates, 0, candidates.size(), ans, new ArrayList<Integer>());
     return ans;
   }
   
-  private void findCombinationa(int target, int[] candidates, int s, int e, ArrayList<List<Integer>> ans, ArrayList<Integer> combo) {
-    if (target < 0) return;
-    
-    if (target == 0) {
-      ans.add((ArrayList) combo.clone());
+  private List<Integer> createCandidates(int n) {
+    ArrayList<Integer> candidates = new ArrayList<>();
+    for (int i = 1; i < 10 && i <= n; i++) candidates.push(i);
+    return candidates;
+  }
+  
+  private void findCombinationa(int k, int target, List<Integer> candidates, int s, int e, ArrayList<List<Integer>> ans, ArrayList<Integer> combo) {
+    if (k == 0 || target < 0) {
+      if (target == 0) {
+        ans.add((ArrayList) combo.clone());
+      }
       return;
     }
     
     for (int i = s; i < e; i++) {
-      int v = candidates[i];
+      int v = candidates.get(i);
       
       if (v > target) break;
       
-      if (i != s && v == candidates[i-1]) {
+      if (i != s && v == candidates.get(i-1)) {
       	continue;
       }
       
       combo.add(v);
-      this.findCombinationa(target - v, candidates, i + 1, e, ans, combo);
+      this.findCombinationa(k-1, target-v, candidates, i + 1, e, ans, combo);
       combo.remove(combo.size()-1);
     }
   }
