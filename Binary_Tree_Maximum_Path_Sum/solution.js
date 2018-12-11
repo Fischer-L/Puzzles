ae3fecb961ea532b9324b51a0b7fb8dc56ef11ea
Fischer-L <Fischer-L@users.noreply.github.com>
2018-12-11 18:55:24 +0800

    Update solution.js

--- Binary_Tree_Maximum_Path_Sum/solution.js
+++ Binary_Tree_Maximum_Path_Sum/solution.js
@@ -10,5 +10,22 @@
  * @return {number}
  */
 var maxPathSum = function(root) {
-    
+    let maxSum = Number.MIN_SAFE_INTEGER;
+    function dfs(root) {
+        if (!root) return 0;
+        
+        let leftMax = dfs(root.left);
+        let rightMax = dfs(root.right);
+        
+        let maxHere = root.val;
+        let maxThruLeft = maxHere + leftMax;
+        let maxThruRight = maxHere + rightMax;
+        let maxThruBoth = maxThruLeft + rightMax;
+        
+        maxHere = Math.max(maxHere, maxThruLeft, maxThruRight);
+        maxSum = Math.max(maxSum, maxHere, maxThruBoth);
+        return maxHere;
+    }
+    dfs(root);
+    return maxSum;
 };
