5bdea7517932feb02ae7f3c8612af4edfb1205b9
Fischer-L <Fischer-L@users.noreply.github.com>
2018-12-12 16:06:32 +0800

    Update solution.java

--- Longest_Increasing_Path_in_a_Matrix/solution.java
+++ Longest_Increasing_Path_in_a_Matrix/solution.java
@@ -1,50 +1,32 @@
-// Long execution time version
 class Solution {
-  private int longest = 0;
-  
   public int longestIncreasingPath(int[][] matrix) {
     int MAX_ROW = matrix == null ? 0 : matrix.length;
     int MAX_COL = MAX_ROW == 0 ? 0 : matrix[0].length;
+    int[][] dp = new int[MAX_ROW][MAX_COL];
+    int longest = 0;
     for (int row = 0; row < MAX_ROW; row++) {
       for (int col = 0; col < MAX_COL; col++) {
-        if (!this.isVisitedSmaller(matrix, row, col, MAX_ROW, MAX_COL)) {
-          this.search(Integer.MIN_VALUE, 0, matrix, row, col, MAX_ROW, MAX_COL);
-        }
+        longest = Math.max(longest, this.search(Integer.MIN_VALUE, dp, matrix, row, col, MAX_ROW, MAX_COL));
       }
     }
-    return this.longest;
+    return longest;
   }
   
-  private boolean isVisitedSmaller(int[][] matrix, int row, int col, int MAX_ROW, int MAX_COL) {
-    int value = matrix[row][col];
-    int i = row - 1;
-    int j = col;
-    if ((0 <= i && i < MAX_ROW) && (0 <= j && j < MAX_COL)) {
-      if (matrix[i][j] < value) return true;
-    }
-    i = row;
-    j = col - 1;
-    if ((0 <= i && i < MAX_ROW) && (0 <= j && j < MAX_COL)) {
-      if (matrix[i][j] < value) return true;
-    }
-    return false;
-  }
-  
-  private void search(int preValue, int path, int[][] matrix, int row, int col, int MAX_ROW, int MAX_COL) {
-    if (row < 0 || row >= MAX_ROW) return;
-    if (col < 0 || col >= MAX_COL) return;
+  private int search(int preValue, int[][] dp, int[][] matrix, int row, int col, int MAX_ROW, int MAX_COL) {
+    if (row < 0 || row >= MAX_ROW) return 0;
+    if (col < 0 || col >= MAX_COL) return 0;
     
     int value = matrix[row][col];
-    if (value <= preValue) return;
+    if (preValue >= value) return 0;
     
-    path++;
-    this.longest = Math.max(this.longest, path);
+    if (dp[row][col] > 0) return dp[row][col];
     
-    matrix[row][col] = Integer.MIN_VALUE;
-    this.search(value, path, matrix, row-1, col, MAX_ROW, MAX_COL);
-    this.search(value, path, matrix, row+1, col, MAX_ROW, MAX_COL);
-    this.search(value, path, matrix, row, col-1, MAX_ROW, MAX_COL);
-    this.search(value, path, matrix, row, col+1, MAX_ROW, MAX_COL);
-    matrix[row][col] = value;
+    int path = 0;
+    path = Math.max(path, this.search(value, dp, matrix, row-1, col, MAX_ROW, MAX_COL));
+    path = Math.max(path, this.search(value, dp, matrix, row+1, col, MAX_ROW, MAX_COL));
+    path = Math.max(path, this.search(value, dp, matrix, row, col-1, MAX_ROW, MAX_COL));
+    path = Math.max(path, this.search(value, dp, matrix, row, col+1, MAX_ROW, MAX_COL));
+    dp[row][col] = 1 + path;
+    return dp[row][col];
   }
 }
