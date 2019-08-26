2fadf146d466c32df1beea4417aa55b4151bbbb3
Fischer-L <Fischer-L@users.noreply.github.com>
2019-08-27 01:15:29 +0800

    Update solution.java

--- GCD_of_Numbers/solution.java
+++ GCD_of_Numbers/solution.java
@@ -1,4 +1,18 @@
 class Solution {
   int gcd(int[] nums) {
+    final int L = nums.length;
+    if (L == 2) return this._gcd(nums[0], nums[1]);
+    
+    int mod = Math.abs(nums[0] - nums[1]);
+    for (let i = 2; i < L - 1; i++) {
+      mod = Math.abs(nums[i] - mod);
+    }
+    return this._gcd(nums[L-1], mod);
+  }
+  
+  int _gcd(int a, int b) {
+    if (a == b) return a;
+    if (a > b) return this._gcd(a - b, b);
+    return this._gcd(a, b - a);
   }
 }
