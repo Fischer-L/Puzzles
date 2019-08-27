995a2a7fbd1722b929534e0efc7bc23cf8bc5765
Fischer-L <Fischer-L@users.noreply.github.com>
2019-08-28 00:09:25 +0800

    Create solution.js

---
+++ Insert_into_a_Binary_Search_Tree/solution.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function(root, val) {
+  if (!root) {
+    return { 
+      val,
+      left: null,
+      right: null,
+    };
+  }
+  
+  if (root.val > val) {
+    root.left = insertIntoBST(root.left, val);
+  } else {
+    root.right = insertIntoBST(root.right, val);
+  }
+  return root;
+};
