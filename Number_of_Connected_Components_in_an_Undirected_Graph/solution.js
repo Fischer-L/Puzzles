091f573aafca21f301cbecd481cd8faa24029462
Fischer-L <Fischer-L@users.noreply.github.com>
2022-08-06 01:11:27 +0800

    Create solution.js

---
+++ Number_of_Connected_Components_in_an_Undirected_Graph/solution.js
@@ -0,0 +1,39 @@
+function numberOfConnectedComponents (n, edges) {
+  const uf = new UnionFind(n);
+  edges.forEach((a, b) => uf.union(a, b));
+  return uf.count;
+}
+
+class UnionFind {
+  constructor (n) {
+    this._count = n;
+    this._parent = Array(n).fill(0).map((_, i) => i);
+  }
+  
+  union (a, b) {
+    const rootA = this.find(a);
+    const rootB = this.find(b);
+    if (rootA === rootB) {
+      return;
+    }
+    this._parent[b] = rootA;
+    this._count--;
+  }
+  
+  connected (a, b) {
+    const rootA = this.find(a);
+    const rootB = this.find(b);
+    return rootA === rootB;
+  }
+  
+  find (a) {
+    if (this._parent[a] !== a) {
+      this._parent[a] = this.find(this._parent[a]);
+    }
+    return this._parent[a];
+  }
+  
+  get count () {
+    return this._count;
+  }
+}
