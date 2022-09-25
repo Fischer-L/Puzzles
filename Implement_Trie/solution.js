ac656336db572b5e14414464ce3d91f8b70a7ab8
Fischer-L <Fischer-L@users.noreply.github.com>
2022-09-26 02:20:06 +0800

    Create solution.js

---
+++ Implement_Trie/solution.js
@@ -0,0 +1,39 @@
+
+class TrieNode {
+  constructor ({ char = '', word = '', isRoot = false, parent = null  } = {}) {
+    this.char = char;
+    this.word = word;
+    this.isRoot = isRoot;
+    this.parent = parent;
+    this.children = {};
+  }
+}
+
+class Trie {
+  constructor () {
+    this.root = new TrieNode({ isRoot: true });
+  }
+  
+  insert (word) {
+    this._insert(this.root, word, 0);
+  }
+  
+  _insert (node, word, i) {
+    const N = word.length;
+    if (i >= N) {
+      return;
+    }
+    
+    const c = word[i];
+    let child = node.children[c];
+    if (!child) {
+      child = new TrieNode({ char: c, parent: node });
+    }
+    if (i === N - 1) {
+      child.word = word;
+    }
+    node.children[c] = child;
+    
+    this._insert(child, word, i + 1);
+  }
+}
