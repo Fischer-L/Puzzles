d32ab68f2408834e8cc02eeb0132f4582430ae6c
Fischer-L <Fischer-L@users.noreply.github.com>
2024-09-23 23:59:19 +0800

    Update solution.js

--- MAX_MEX/solution.js
+++ MAX_MEX/solution.js
@@ -1,3 +1,24 @@
-function solution (nums) {
-  
+function solution (nums, x) {
+  const numFreq = Array(nums.length + 1).fill(0);
+  nums.forEach(n => numFreq[n % x]++);
+
+  for (let i = 1; i < numFreq.length; i++) {
+    if (numFreq[i]) {
+      // The number i exists
+      continue;
+    }
+    if (numFreq[i % x] > 1) {
+      // Assume x is 3, the number 5 with no freqs and the number 2 has 2 freqs.
+      // We can produce a number 5 by adding 3 with one number 2.
+      numFreq[i % x]--;
+      numFreq[i]++;
+    }
+  }
+
+  for (let i = numFreq.length - 1; i >= 1; i++) {
+    if (!numFreq[i]) {
+      return i;
+    }
+  }
+  return numFreq.length;
 }
