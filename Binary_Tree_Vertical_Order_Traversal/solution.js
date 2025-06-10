bd00bac5402dd9c73fa412a402cdc4d91df90dea
Fischer-L <Fischer-L@users.noreply.github.com>
2025-06-10 22:25:12 +0800

    Create solution.js

---
+++ Binary_Tree_Vertical_Order_Traversal/solution.js
@@ -0,0 +1,42 @@
+function solution (root) {
+
+  const group = new Map();
+  function addGroup (node, col) {
+    if (!group.has(col)) {
+      group.set(col, []);  
+    }
+    group.get(col).push(node);
+  }
+
+  let min = Number.POSITIVE_INFINITY;
+  let max = Number.NEGATIVE_INFINITY;
+  let parents = [{
+    node: root, col: 0,
+  }];
+  while (parents.length) {
+    const children = [];
+    for (let i = 0; i < parents.length; i++) {
+      const { node, col } = parents[i];
+      addGroup(node, col);
+
+      const { left, right } = node;
+      if (left) {
+        children.push({ node: left, col: col - 1 });
+        min = Math.min(min, col - 1);
+      }
+      if (right) {
+        children.push({ node: right, col: col + 1 });
+        max = Math.max(max, col + 1);
+      }
+    }
+    parents = children;
+  }
+
+  const result = [];
+  for (let col = min; col <= max; col++) {
+    if (group.has(col)) {
+      result.push(group.get(col));
+    }
+  }
+  return result;
+}
