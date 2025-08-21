aa573787cf9ce256a42cb0fb13c1be2b7dde7516
Fischer-L <Fischer-L@users.noreply.github.com>
2025-08-21 23:15:08 +0800

    Update solution.js

--- Heap/solution.js
+++ Heap/solution.js
@@ -1,117 +1,96 @@
 class HeapBase {
-  constructor (cmp) {
-    if (typeof cmp === 'function') {
-      this._cmp = cmp;
-    }
-    
-    this._q = [ null ];
+
+  constructor () {
+    this._vals = [ null ];
+  }
+
+  get size () {
+    return this._vals.length - 1;
+  }
+
+  _left (i) {
+    i = i * 2;
+    return i <= this.size ? i : null;
+  }
+
+  _right (i) {
+    i = i * 2 + 1;
+    return i <= this.size ? i : null;
   }
 
   _parent (i) {
     return Math.floor(i / 2);
   }
 
-  _left (i) {
-    return i * 2;
+  _swap (i , j) {
+    const tmp = this._vals[i];
+    this._vals[i] = this._vals[j];
+    this._vals[j] = tmp;
   }
 
-  _right (i) {
-    return i * 2 + 1;
-  }
-
-  _swap (i, j) {
-    const tmp = this._q[i];
-    this._q[i] = this._q[j];
-    this._q[j] = tmp;
-  }
-  
-  _cmp (a, b) {
-    return a - b;
-  }
-
-  get size () {
-    return this._q.length - 1;
-  }
-
-  push (v) {
-    this._q.push(v);
-    this._swim(this.size);
+  _isPrior (i, j) {
+    throw new Error('Should implement _isPrior');
   }
 
   peek () {
-    return this.size ? this._q[1] : null;
+    return this.size > 0 ? this._vals[1] : null;
   }
 
   pop () {
     const v = this.peek();
     if (v !== null) {
       this._swap(1, this.size);
-      this._q.pop();
+      this._vals.pop();
       this._sink(1);
     }
     return v;
   }
+
+  _sink (i) {
+    while (1 <= i && i < this.size) {
+      const l = this._left(i);
+      const r = this._right(i);
+      let t = l;
+      if (r && this._isPrior(r, t)) {
+        t = r;
+      }
+      if (t && this._isPrior(t, i)) {
+        this._swap(i, t);
+        i = t;
+      } else {
+        break;
+      }
+    }
+  }
+
+  push (v) {
+    this._vals.push(v);
+    this._swim(this.size);
+  }
+
+  _swim (i) {
+    while (1 < i && i <= this.size) {
+      const p = this._parent(i);
+      if (this._isPrior(i, p)) {
+        this._swap(i, p);
+        i = p;
+      } else {
+        break;
+      }
+    }
+  }
 }
 
 class MinHeap extends HeapBase {
-  _swim (i) {
-    while (i >= 2) {
-      const p = this._parent(i);
-      if (this._cmp(this._q[i], this._q[p]) < 0) {
-        this._swap(i, p);
-        i = p;
-      } else {
-        break;
-      }
-    }
-  }
 
-  _sink (i) {
-    while (i < this.size) {
-      const l = this._left(i);
-      const r = this._right(i);
-      let min = l;
-      if (min <= this.size && r <= this.size && this._cmp(this._q[r], this._q[min]) < 0) {
-        min = r;
-      }
-      if (min <= this.size && this._cmp(this._q[min], this._q[i]) < 0) {
-        this._swap(i, min);
-        i = min;
-      } else {
-        break;
-      }
-    }
+  _isPrior (i, j) {
+    return this._vals[i] < this._vals[j];
   }
 }
 
-
 class MaxHeap extends HeapBase {
-  _swim (i) {
-    while (i >= 2) {
-      const p = this._parent(i);
-      if (this._cmp(this._q[i], this._q[p]) > 0) {
-        this._swap(i, p);
-        i = p;
-      } else {
-        break;
-      }
-    }
-  }
 
-  _sink (i) {
-    while (i < this.size) {
-      const l = this._left(i);
-      const r = this._right(i);
-      let max = l ;
-      if (max <= this.size && r <= this.size && this._cmp(this._q[r], this._q[max]) > 0) {
-        max = r;
-      }
-      if (max <= this.size && this._cmp(this._q[max], this._q[i]) > 0) {
-        this._swap(i, max);
-        i = max;
-      } else {
-        break;
-      }
-    } 
+  _isPrior (i, j) {
+    return this._vals[i] > this._vals[j];
   }
 }
